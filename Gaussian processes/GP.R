b0571d608d694d8e6b0231cd53cd841758c5db27
josefwilzen <josefwilzen@gmail.com>
2014-04-07 10:46:04 +0200

    updates

--- Gaussian processes/GP.R
+++ Gaussian processes/GP.R
@@ -1,4 +1,5 @@
-
+rm(list=ls())
+source("Gaussian processes/GPfunc.R")
 
 #draw<-sampleGP(noDraw=20,xVal=seq(-6,6,length.out=100),covFunc=SqrExpCov,lengthScale=2,sigma=1,func=sin)
 #draw<-sampleGP(noDraw=20,xVal=seq(-6,6,length.out=100),covFunc=SqrExpCov,lengthScale=2,sigma=1,func=sin,onlyPara=T)
@@ -14,45 +15,42 @@ plot(wages$age,wages$logWage,pch=20,ylim=c(10,17),col="blue")
 # calculate posterior
 
 # prior:
-# let l=0.5 and sigma=0.4 (change those later)
+# let l=0.2 and sigma=1 (change those later)
 # assume sigmaError to be sd of data
+priorSigma<-1
+priorLengthScale<-0.2
+
 sigmaError<-sd(wages$logWage)
-xGrid<-seq(-1.5,2.2,length=30)
-draws<-sampleGP(noDraw=30,xVal=xGrid,covFunc=SqrExpCov,func=meanWages,sigma=0.4,lengthScale=0.5)
+xGrid<-seq(-1.5,2.2,length=300)
+draws<-sampleGP(noDraw=30,xVal=xGrid,covFunc=SqrExpCov,func=meanWages,sigma=priorSigma,lengthScale=priorLengthScale)
+# prior mean function:
 plotGP(draws$draws)
 curve(expr=meanWages,from=-1.7,to=2.3,ylim=c(10,15),add=TRUE,lwd=4,col="red")
 
+
+#plot(wages$age,wages$logWage,pch=20,ylim=c(10,17),,xlim=c(-1.7,2.3),col="blue",xlab="age",ylab="log(wages)")
+#curve(expr=meanWages,from=-1.7,to=2.3,ylim=c(10,15),add=TRUE,lwd=3)
+
+
+test<-posteriorDist(xGrid=xGrid,priorMean=meanWages,obsData=wages,covFunc=SqrExpCov,sigmaError=sigmaError,sigma=priorSigma,lengthScale=priorLengthScale)
+
+plotTheoreticalProbBand(postDist=test,predictiveMean=FALSE,plotData=TRUE)
+plotTheoreticalProbBand(postDist=test,predictiveMean=TRUE,plotData=TRUE)
+
+
+
+
+library(MASS)
+drawsPosterior<-mvrnorm(n=2000,mu=fBar,Sigma=posteriorCov)
+colnames(drawsPosterior)<-round(xGrid,3)
 # prior mean function:
-plot(wages$age,wages$logWage,pch=20,ylim=c(10,17),,xlim=c(-1.7,2.3),col="blue",xlab="age",ylab="log(wages)")
-curve(expr=meanWages,from=-1.7,to=2.3,ylim=c(10,15),add=TRUE,lwd=3)
+plotGP(drawsPosterior)
+lines(x=xGrid,y=fBar,col="red",lwd=3)
+plotProbBandsGP(drawMatrix=drawsPosterior,postMean=fBar,xGrid=xGrid)
 
-library(lubridate)
-# posterior mean
-# grid of xValues
-xGrid<-seq(-1.5,2.2,length=30)
+#plot(x=xGrid,y=fBar,type="l",main=as.character(now()))
+conf<-apply(X=drawsPosterior,MARGIN=2,FUN=quantile,prob=c(0.05,0.95))
 
-meanXgrid<-as.matrix(meanFunc(xValues=xGrid,func=meanWages))
-xKxGrid<-createCovMatrix(xVal=wages$age,yVal=xGrid,covFunc=SqrExpCov,sigma=0.4,lengthScale=0.5)
-sqrtKy<-sqrt(createCovMatrix(xVal=wages$age,covFunc=SqrExpCov,sigma=0.4,lengthScale=0.5)
-             +((sigmaError)^2)*diag(1,nrow=dim(wages)[1]))
-yDiff<-wages$logWage-meanFunc(xValues=wages$age,func=meanWages)
-#fBar<-xKxGrid%*%sqrtKy%*%wages$logWage
-fBar<-meanXgrid+xKxGrid%*%sqrtKy%*%wages$logWage
-#plot(x=wages$age,y=meanFunc(xValues=wages$age,func=meanWages),type="l")
-#plot(x=xGrid,y=meanXgrid,type="l")
-graphics.off()
-
-plot(x=xGrid,y=fBar,type="o",main=as.character(now()))
-
-
-temp3<-createCovMatrix(xVal=xGrid,yVal=wages$age,covFunc=SqrExpCov)
-temp2<-createCovMatrix(xVal=wages$age,yVal=xGrid,covFunc=SqrExpCov)
-temp1<-createCovMatrix(xVal=wages$age,covFunc=SqrExpCov)
-temp4<-createCovMatrix(xVal=xGrid,covFunc=SqrExpCov)
-dim(temp2)
-dim(temp3)
-dim(temp1)
-dim(temp4)
 
 
 
