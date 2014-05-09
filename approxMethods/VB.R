ae8c7ddb21907d290a1ac0ec634c2c3ec963da45
josefwilzen <josefwilzen@gmail.com>
2014-05-09 16:32:28 +0200

    updates

--- approxMethods/VB.R
+++ approxMethods/VB.R
@@ -4,7 +4,9 @@
 #-------------------------------------------------------------------------
 #-------------------------------------------------------------------------
 
-
+#install.packages("gridExtra")
+library(gridExtra)
+library(ggplot2)
 
 #-------------------------------------------------------------------------
 # b)
@@ -16,71 +18,102 @@ dataVect<-c(1,0,3,4,2,6,2,3,1,1)
 
 source("/home/joswi05/Dropbox/Josef/Advanced Bayesian Learning/AdvBayesianLearning/approxMethods/VBfunc.R")
 
-test<-gibbsHierarchialPoisson(lambdaStart=1,betaStart=1,obsData=dataVect,mySeed=4567,nObs=5000)
+test<-gibbsHierarchialPoisson(lambdaStart=1,betaStart=1,obsData=dataVect,mySeed=49067,nObs=1000)
+pGibbs1<-ggplot(data=as.data.frame(test),aes(x=lambda,y=beta))+geom_point()+theme_bw()+geom_density2d()+xlab(expression(lambda)) +ylab(expression(beta))
+pGibbs2<-ggplot(as.data.frame(test), aes(lambda)) +geom_density(alpha = 0.2)+theme_bw()+xlab(expression(lambda)) 
+pGibbs3<-ggplot(as.data.frame(test), aes(beta)) +geom_density(alpha = 0.2)+theme_bw()+xlab(expression(beta))
+grid.arrange(pGibbs2,pGibbs3,pGibbs1, ncol=2)
 
-plot(test)
-plot(test[,1],type="l")
-plot(test[,2],type="l")
-plot(density(test[,1]),main="lambda")
-plot(density(test[,2]),main="beta")
-cov(x=test[,1],y=test[,2])
-
-library(ggplot2)
-pl<-ggplot(data=as.data.frame(test),aes(x=lambda,y=beta))+geom_point()+theme_bw()+
-  geom_density2d()+xlab(expression(lambda)) +ylab(expression(beta))
-print(pl)
-
-#pl+scale_fill_gradient(limits=c(1e-5,8e-4))
-#pl+stat_density2d(geom="point", aes(size = ..density..), contour = FALSE)
-
-par(mfrow=c(2,2))
-plot(density(test[,1]),main="lambda")
-plot(density(test[,2]),main="beta")
-#plot(test)
-par(mfrow=c(1,1))
-example(stat_density2d)
 
 #-------------------------------------------------------------------------
 # c)
 
 test4<-vb(obsData=dataVect,mySeed=20939)
-pl<-ggplot(data=as.data.frame(test4),aes(x=lambda,y=beta))+geom_point()+theme_bw()+
-  geom_density2d()+xlab(expression(lambda)) +ylab(expression(beta))
+pl<-ggplot(data=as.data.frame(test4),aes(x=lambda,y=beta))+geom_point()+theme_bw()+geom_density2d()+xlab(expression(lambda)) +ylab(expression(beta))
+plVB1<-ggplot(as.data.frame(test4), aes(lambda)) +geom_density(alpha = 0.2)+theme_bw()+xlab(expression(lambda)) 
+plVB2<-ggplot(as.data.frame(test4), aes(beta)) +geom_density(alpha = 0.2)+theme_bw()+xlab(expression(beta))
+grid.arrange(plVB1,plVB2,pl, ncol=2)
+
+
+#--------------------------------------------
+# computing times:
+
+system.time(test1<-gibbsHierarchialPoisson(lambdaStart=1,betaStart=1,obsData=dataVect,mySeed=49067,nObs=1000))
+system.time(test2<-vb(obsData=dataVect,mySeed=20939))
+
+#--------------------------------------------
+# comparing:
+
+testVB<-data.frame(test4,Method="VB")
+testGibbs<-data.frame(test,Method="Gibbs")
+testTotal<-rbind(testGibbs,testVB)
+
+totLambda<-ggplot(testTotal, aes(x=lambda, color=Method)) + geom_density()+theme_bw()+xlab(expression(lambda))
+totBeta<-ggplot(testTotal, aes(x=beta, color=Method)) + geom_density()+theme_bw()+xlab(expression(lambda))
+grid.arrange(totLambda,totBeta,ncol=2)
+
+#ggplot(df, aes(x=rating, fill=cond)) + geom_density(alpha=.3)
+
+pl<-ggplot(data=as.data.frame(testTotal),aes(x=lambda,y=beta,col=Method))+geom_point()+theme_bw()+geom_density2d()+xlab(expression(lambda)) +ylab(expression(beta))
 print(pl)
-
-plVB1<-ggplot(as.data.frame(test4), aes(lambda)) +geom_density(alpha = 0.2)+theme_bw()
-plVB2<-ggplot(as.data.frame(test4), aes(beta)) +geom_density(alpha = 0.2)+theme_bw()
-multiplot(plVB1,plVB2)
-
+testTotal
 
 #-------------------------------------------------------------------------
 #  ABC
-function(x){return(sum(log(x)))
-
-test2<-likeFreeRejectionSample2(obsData=dataVect,tol=1e-5,nObs=1000,statistic=mad,distFunc=tempFunc)
-plot(density(test2[,1]),main="lambda")
-plot(density(test2[,2]),main="beta")
-pl<-ggplot(data=as.data.frame(test2),aes(x=lambda,y=beta))+geom_point()+theme_bw()+
-  geom_density2d()+xlab(expression(lambda)) +ylab(expression(beta))
-print(pl)
-
-tempFunc<-function(x){
-  y<-sum(abs(x))
-  return(y)
-}
 
 
-par(mfrow=c(2,2))
-plot(density(test[,1]),main="lambda")
-plot(density(test[,2]),main="beta")
-plot(density(test2[,1]),main="lambda")
-plot(density(test2[,2]),main="beta")
-par(mfrow=c(1,1))
-dist(x=cbind(2:3,20:21))
+# a)
+
+test2<-likeFreeRejectionSample2(obsData=dataVect,tol=1,nObs=1000,statistic=sum,mySeed=2344)
+pReject<-ggplot(data=as.data.frame(test2),aes(x=lambda,y=beta))+geom_point()+theme_bw()+geom_density2d()+xlab(expression(lambda)) +ylab(expression(beta))
+pReject1<-ggplot(as.data.frame(test2), aes(lambda)) +geom_density(alpha = 0.2)+theme_bw()+xlab(expression(lambda)) 
+pReject2<-ggplot(as.data.frame(test2), aes(beta)) +geom_density(alpha = 0.2)+theme_bw()+xlab(expression(beta))
+grid.arrange(pReject1,pReject2,pReject, ncol=2)
+summary(test2[,3])
+
+
+test3<-likeFreeRejectionSample2(obsData=dataVect,tol=1,nObs=1000,statistic=mean,mySeed=NULL)
+pReject<-ggplot(data=as.data.frame(test4),aes(x=lambda,y=beta))+geom_point()+theme_bw()+geom_density2d()+xlab(expression(lambda)) +ylab(expression(beta))
+pReject1<-ggplot(as.data.frame(test4), aes(lambda)) +geom_density(alpha = 0.2)+theme_bw()+xlab(expression(lambda)) 
+pReject2<-ggplot(as.data.frame(test4), aes(beta)) +geom_density(alpha = 0.2)+theme_bw()+xlab(expression(beta))
+grid.arrange(pReject1,pReject2,pReject, ncol=2)
+summary(test2[,3])
+
+
+rejection1<-likeFreeRejectionSample2(obsData=dataVect,tol=0,nObs=1000,statistic=mean,mySeed=2344)
+rejection2<-likeFreeRejectionSample2(obsData=dataVect,tol=0,nObs=1000,statistic=sum,mySeed=3444)
+rejection3<-likeFreeRejectionSample2(obsData=dataVect,tol=0,nObs=1000,statistic=median,mySeed=3554)
+rejection4<-likeFreeRejectionSample2(obsData=dataVect,tol=0,nObs=1000,statistic=range,mySeed=34006)
+
+plRejection1<-ggplot(data=as.data.frame(rejection1),aes(x=lambda,y=beta))+geom_point()+theme_bw()+geom_density2d()+xlab(expression(lambda)) +ylab(expression(beta))
+plRejection2<-ggplot(data=as.data.frame(rejection2),aes(x=lambda,y=beta))+geom_point()+theme_bw()+geom_density2d()+xlab(expression(lambda)) +ylab(expression(beta))
+plRejection3<-ggplot(data=as.data.frame(rejection3),aes(x=lambda,y=beta))+geom_point()+theme_bw()+geom_density2d()+xlab(expression(lambda)) +ylab(expression(beta))
+plRejection4<-ggplot(data=as.data.frame(rejection4),aes(x=lambda,y=beta))+geom_point()+theme_bw()+geom_density2d()+xlab(expression(lambda)) +ylab(expression(beta))
+grid.arrange(plRejection1,plRejection2,plRejection3,plRejection4, ncol=2)
 
 
 
+rejection5<-likeFreeRejectionSample2(obsData=dataVect,tol=0,nObs=1000,statistic=mean,mySeed=2144)
+rejection6<-likeFreeRejectionSample2(obsData=dataVect,tol=3,nObs=1000,statistic=mean,mySeed=22444)
+rejection7<-likeFreeRejectionSample2(obsData=dataVect,tol=6,nObs=1000,statistic=mean,mySeed=355411)
+rejection8<-likeFreeRejectionSample2(obsData=dataVect,tol=9,nObs=1000,statistic=mean,mySeed=34756)
+
+plRejection5<-ggplot(data=as.data.frame(rejection5),aes(x=lambda,y=beta))+geom_point()+theme_bw()+geom_density2d()+xlab(expression(lambda)) +ylab(expression(beta))
+plRejection6<-ggplot(data=as.data.frame(rejection6),aes(x=lambda,y=beta))+geom_point()+theme_bw()+geom_density2d()+xlab(expression(lambda)) +ylab(expression(beta))
+plRejection7<-ggplot(data=as.data.frame(rejection7),aes(x=lambda,y=beta))+geom_point()+theme_bw()+geom_density2d()+xlab(expression(lambda)) +ylab(expression(beta))
+plRejection8<-ggplot(data=as.data.frame(rejection8),aes(x=lambda,y=beta))+geom_point()+theme_bw()+geom_density2d()+xlab(expression(lambda)) +ylab(expression(beta))
+grid.arrange(plRejection5,plRejection6,plRejection7,plRejection8, ncol=2)
 
 
+testSample<-likeFreeRejectionSample2(obsData=dataVect,tol=0,nObs=1000,statistic=mean,mySeed=22144,storeData=TRUE)
 
+str(testSample)
 
+#--------------------------------------------------------------------------------
+# b)
+a<-likeFreeMCMC(obsData=dataVect,tuning=0.1,statistic=mean,nObs=1000,tol=1,mySeed=2873)
+
+plot(a$parameters[,1],type="l")
+plot(a$parameters[,2],type="l")
+aPlot<-ggplot(data=as.data.frame(a$parameters[,1:2]),aes(x=lambda,y=beta))+geom_point()+theme_bw()+geom_density2d()+xlab(expression(lambda)) +ylab(expression(beta))
+print(aPlot)
--- approxMethods/VBfunc.R
+++ approxMethods/VBfunc.R
@@ -15,14 +15,14 @@ gibbsHierarchialPoisson<-function(lambdaStart,betaStart,obsData,mySeed=NULL,nObs
   colnames(draws)<-c("lambda","beta")
   sumStat<-sum(obsData)
   n<-length(obsData)
-  aLambda<-sumStat+n*alpha+n+1
-  aBeta<-n*psi-n+1
+  aLambda<-sumStat+alpha
+  aBeta<-psi
   for(i in 2:dim(draws)[1]){
     # cond. for lambda:
-    bLabmda<-n+draws[(i-1),2]*n
+    bLabmda<-n+draws[(i-1),2]
     draws[i,1]<-rgamma(n=1,shape=aLambda,rate=bLabmda)
     # cond. for beta:
-    bBeta<-n*draws[(i-1),1]+n*theta
+    bBeta<-draws[(i-1),1]+theta
     draws[i,2]<-rgamma(n=1,shape=aBeta,rate=bBeta)
   }
   draws<-draws[-1,]
@@ -36,7 +36,7 @@ gibbsHierarchialPoisson<-function(lambdaStart,betaStart,obsData,mySeed=NULL,nObs
 
 
 
-vbParameterEstimate<-function(obsData,myB0=2,nIter=20,psi=1,alpha=1,theta=5){
+vbParameterEstimate<-function(obsData,myB0=2,nIter=50,psi=1,alpha=1,theta=5){
   sumX<-sum(obsData)
   n<-length(obsData)
   aLambda<-sumX+alpha
@@ -81,43 +81,133 @@ vb<-function(obsData,noDraws=1000,mySeed=NULL,...){
 #-------------------------------------------------------------------------
 # a) 
 
-likeFreeRejectionSample2<-function(obsData,statistic=sum,distFunc=abs,nObs=1000,tol=0.0001,psi=1,beta=1,theta=5,...){
-  result<-matrix(0,nrow=nObs,ncol=2)
-  colnames(result)<-c("lambda","beta")
+
+
+euclideanDist<-function(x,y){
+  z<-sqrt(sum((x-y)^2))
+  return(z)
+}
+
+
+
+likeFreeRejectionSample2<-function(obsData,statistic=sum,nObs=1000,tol=1,psi=1,beta=1,theta=5,alpha=1,mySeed=NULL,storeData=FALSE,...){
+   set.seed(mySeed)
+  result<-matrix(0,nrow=nObs,ncol=3)
+  colnames(result)<-c("lambda","beta","count")
   yStat<-statistic(obsData,...)
+  n<-length(obsData)
+  resultData<-matrix(0,nrow=nObs,ncol=n)
   for(i in 1:nObs){
-    
     # generate the first proposal:
     beta<-rgamma(n=1,shape=psi,rate=theta)
     lambda<-rgamma(n=1,shape=alpha,rate=beta)
     z<-rpois(n=10,lambda=lambda)
     zStat<-statistic(z,...)
-    count<-0
-    while(distFunc(yStat-zStat)>tol){
-      #print(paste("Dist: ",distFunc(yStat-zStat)))
-      beta<-rgamma(n=1,shape=psi,rate=5)
+    count<-1
+    while(euclideanDist(x=zStat,y=yStat)>tol){
+      beta<-rgamma(n=1,shape=psi,rate=theta)
       lambda<-rgamma(n=1,shape=alpha,rate=beta)
-      z<-rpois(n=10,lambda=lambda)
+      z<-rpois(n=n,lambda=lambda)
       zStat<-statistic(z,...)
       count<-count+1
     }
-    tempDist<-distFunc(yStat-zStat)
-    result[i,]<-c(lambda,beta)
+    tempDist<-euclideanDist(x=zStat,y=yStat)
+    result[i,]<-c(lambda,beta,count)
+    resultData[i,]<-z
     print(paste("i: ",i," count: ",count,"tempDist: ",tempDist))
-    count<-1
-    
+    count<-1 
   }
-
+  if(storeData){
+    res<-list(para=result,dataSample=resultData)
+    return(res)
+  }
   return(result)
+}
+
+library(MASS)
+library(mvtnorm)
+
+posterior<-function(paraVect,obsData,psi=1,theta=5,alpha=1){
+  # lambda first elemnt of paraVect and beta second element of paraVect
+  lambda<-paraVect[1]
+  beta<-paraVect[2]
+  n<-length(obsData)
+  postDens<-lambda^(sum(obsData))*exp(-lambda*n)*lambda^(alpha-1)*exp(-lambda*beta)*beta^(psi-1)*exp(-beta*theta)
+  return(postDens)
 }
 
 
 
+likeFreeMCMC<-function(obsData,tuning,statistic=sum,nObs=1000,tol=1,psi=1,theta=5,alpha=1,mySeed=NULL,...){
+  startObj<-likeFreeRejectionSample2(obsData=obsData,nObs=1,storeData=TRUE,mySeed=mySeed)
+  
+  startPara<-startObj$para[1,1:2]
+  varProp<-tuning*diag(2)
+  set.seed(mySeed)
+  resultPara<-matrix(0,nrow=(nObs+1),ncol=4)
+  resultPara[1,1:2]<-startPara
+  colnames(resultPara)<-c("lambda","beta","all","acceted")
+  yStat<-statistic(obsData,...)
+  n<-length(obsData)
+  resultData<-matrix(0,nrow=(nObs+1),ncol=n)
+  resultData[1,]<-startObj$dataSample[1,]
+  
+  accept<-0
+  
+  for(i in 2:(nObs+1)){
+    #if(i==2) browser()
+    
+    parameterProp<-rmvnorm(n=1,mean=resultPara[(i-1),1:2],sigma=varProp)
+    parameterProp<-ifelse(parameterProp<=0,0.000001,parameterProp)
+    
+    lambda<-rgamma(n=1,shape=alpha,rate=parameterProp[,2])
+    z<-rpois(n=10,lambda=lambda)
+    zStat<-statistic(z,...)
+    u<-runif(1)
+    posteriorOld<-posterior(paraVect=resultPara[(i-1),1:2],obsData=obsData,psi=psi,theta=theta,alpha=alpha)
+    posteriorNew<-posterior(paraVect=parameterProp,obsData=obsData,psi=psi,theta=theta,alpha=alpha)
+    
+    propGivenOld<-dmvnorm(x=parameterProp,mean=resultPara[(i-1),1:2],sigma=varProp)
+    oldGivenProp<-dmvnorm(x=resultPara[(i-1),1:2],mean=parameterProp,sigma=varProp)
+    
+    posteriorRatio<-posteriorNew/posteriorOld
+    proposalRatio<-oldGivenProp/propGivenOld
+    
+    cat("posteriorRatio: ",posteriorRatio,"\n")
+    cond1<-posteriorRatio*proposalRatio>=u
+    cond2<-euclideanDist(x=zStat,y=yStat)<=tol
+    
+    cat("cond1: ",cond1,"cond2: ",cond2,"\n")
+    if(cond1&cond2){
+      resultPara[i,1:2]<-parameterProp
+      resultData[i,]<-z
+    }else{
+      resultPara[i,1:2]<-resultPara[(i-1),1:2]
+      resultData[i,]<-resultData[(i-1),]
+    }
+    
+    # generate the first proposal:
+#     beta<-rgamma(n=1,shape=psi,rate=theta)
+#     lambda<-rgamma(n=1,shape=alpha,rate=beta)
+#     z<-rpois(n=10,lambda=lambda)
+#     zStat<-statistic(z,...)
+#     count<-1
+#     while(euclideanDist(x=zStat,y=yStat)>tol){
+#       beta<-rgamma(n=1,shape=psi,rate=theta)
+#       lambda<-rgamma(n=1,shape=alpha,rate=beta)
+#       z<-rpois(n=n,lambda=lambda)
+#       zStat<-statistic(z,...)
+#       count<-count+1
+#     }
+    tempDist<-euclideanDist(x=zStat,y=yStat)
+    print(paste("i: ",i,"tempDist: ",tempDist))
+    count<-1 
+  }
 
+  res<-list(parameters=resultPara,dataSample=resultData)
 
-
-
-
+  return(res)
+}
 
 
 
